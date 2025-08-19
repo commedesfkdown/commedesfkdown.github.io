00d05c5a51ced36651bbabd2b6848193ed7a2b6e
commedesfkdown <commedesfkdown@icloud.com>
2025-08-19 18:51:35 +0300

    new
    123

---
+++ download/apps.js
@@ -0,0 +1,114 @@
+const appsData = [
+    { 
+        id: "app1", 
+        name: "Telegram", 
+        icon: "fa-brands fa-telegram", 
+        size: "46.6 MB",
+        downloadUrl: "https://telegram.org/dl/desktop/win64",
+        category: "Соц. сети"
+    },
+    { 
+        id: "app2", 
+        name: "Discord", 
+        icon: "fa-brands fa-discord", 
+        size: "124 MB",
+        downloadUrl: "https://discord.com/download",
+        category: "Соц. сети"
+    },
+    { 
+        id: "app3", 
+        name: "Sideloadly", 
+        icon: "fa-brands fa-apple", 
+        size: "142 MB",
+        downloadUrl: "https://sideloadly.io/SideloadlySetup64.exe",
+        category: "Продуктивность"
+    },
+    { 
+        id: "app4", 
+        name: "Chrome", 
+        icon: "fa-brands fa-chrome", 
+        size: "85 MB",
+        downloadUrl: "https://www.google.com/chrome/",
+        category: "Браузеры"
+    },
+    { 
+        id: "app5", 
+        name: "Firefox", 
+        icon: "fa-brands fa-firefox", 
+        size: "72 MB",
+        downloadUrl: "https://www.mozilla.org/ru/firefox/new/",
+        category: "Браузеры"
+    },
+    { 
+        id: "app6", 
+        name: "Photo Pro", 
+        icon: "fa-camera", 
+        size: "48 MB",
+        downloadUrl: "https://example.com/photo-pro.apk",
+        category: "Фото и видео"
+    },
+    { 
+        id: "app7", 
+        name: "Video Master", 
+        icon: "fa-video", 
+        size: "64 MB",
+        downloadUrl: "https://example.com/video-master.apk",
+        category: "Фото и видео"
+    },
+    { 
+        id: "app8", 
+        name: "Яндекс Музыка", 
+        icon: "fa-brands fa-yandex", 
+        size: "32 MB",
+        downloadUrl: "https://music.yandex.ru/client",
+        category: "Музыка"
+    },
+    { 
+        id: "app9", 
+        name: "Spotify", 
+        icon: "fa-brands fa-spotify", 
+        size: "1.5 MB",
+        downloadUrl: "https://www.spotify.com/us/download/windows/",
+        category: "Музыка"
+    },
+    { 
+        id: "app10", 
+        name: "ITunes", 
+        icon: "fa-brands fa-apple", 
+        size: "36 MB",
+        downloadUrl: "https://www.apple.com/itunes/download/win64",
+        category: "Продуктивность"
+    },
+    { 
+        id: "app11", 
+        name: "Edge", 
+        icon: "fa-brands fa-edge", 
+        size: "24 MB",
+        downloadUrl: "https://www.microsoft.com/ru-ru/edge",
+        category: "Браузеры"
+    },
+    { 
+        id: "app12", 
+        name: "Opera GX", 
+        icon: "fa-brands fa-opera", 
+        size: "142 MB",
+        downloadUrl: "https://www.opera.com/gx",
+        category: "Браузеры"
+    },
+    { 
+        id: "app13", 
+        name: "Opera", 
+        icon: "fa-brands fa-opera", 
+        size: "218 MB",
+        downloadUrl: "https://www.opera.com/",
+        category: "Браузеры"
+    },
+    { 
+        id: "app14", 
+        name: "Yandex", 
+        icon: "fa-brands fa-yandex", 
+        size: "218 MB",
+        downloadUrl: "https://browser.yandex.ru/",
+        category: "Браузеры"
+    }
+];
